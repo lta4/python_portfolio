7aa98d13d00caf7a90c3bfbceeac28ec00fd7158
Andrew <andrew.mk4@yahoo.com>
2021-05-20 02:41:03 -0700

    secret key and debug update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g__cu+0h2!_idxqi_!c4rr9rvc^00j)n(j1oidi6bg%=!(3ic@'
+# SECRET_KEY = 'django-insecure-g__cu+0h2!_idxqi_!c4rr9rvc^00j)n(j1oidi6bg%=!(3ic@'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g__cu+0h2!_idxqi_!c4rr9rvc^00j)n(j1oidi6bg%=!(3ic@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
