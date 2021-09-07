526711350511c6469ceb7995bf58fedc4295515b
Andrew <andrew.mk4@yahoo.com>
2021-09-07 02:57:26 -0700

    project_index and project_detail templates success

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from projects.models import Project
 
 # Create your views here.
+from django.shortcuts import render
+from projects.models import Project
+
 def project_index(request):
     projects = Project.objects.all()
     context = {
